422b8fb3ec29c515dc6758a5a773fdeaf3ec3298
Danilo <danilo.colasso@gmail.com>
2024-06-21 22:20:18 -0300

    Add stop distance to target movement

--- Assets/Scripts/Character/Enemy/Enemy.cs
+++ Assets/Scripts/Character/Enemy/Enemy.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class Enemy : Character
 {
     [SerializeField] private Transform target;
+    [SerializeField][Range(0, 10)] private float attackRange = 5f;
 
     protected override void FixedUpdate()
     {
         base.FixedUpdate();
 
         if (target != null) {
-            SetMovement(new TargetMovement(target));
+            SetMovement(new TargetMovement(target, attackRange));
         }
     }
 }
