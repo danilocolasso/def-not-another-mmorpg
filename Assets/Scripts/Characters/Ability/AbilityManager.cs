c2a008f47c5607f95e5c5ea87429a6270083bbfd
Danilo <danilo.colasso@gmail.com>
2024-07-04 03:10:04 -0300

    - Ability range - Ability cooldown - Detection Area disable on start battle

--- Assets/Scripts/Characters/Ability/AbilityManager.cs
+++ Assets/Scripts/Characters/Ability/AbilityManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,28 +6,90 @@ public class AbilityManager : MonoBehaviour
 {
     [SerializeField] private List<Ability> abilities = new List<Ability>();
     private Dictionary<string, Ability> abilityDictionary = new Dictionary<string, Ability>();
+    private Dictionary<Ability, Coroutine> cooldownDictionary = new Dictionary<Ability, Coroutine>();
 
     private void Awake()
     {
         foreach (Ability ability in abilities)
         {
-            if (!abilityDictionary.ContainsKey(ability.name))
-            {
-                abilityDictionary.Add(ability.name, ability);
-                Debug.Log($"Added {ability.name} to Ability Dictionary");
-            }
+            AddAbility(ability);
         }
     }
 
     public void UseAbility(string abilityName, Character caster, Character target)
     {
-        if (abilityDictionary.TryGetValue(abilityName, out Ability ability))
+        if (!abilityDictionary.TryGetValue(abilityName, out Ability ability))
         {
-            ability.Activate(caster, target);
-            Debug.Log($"{caster.name} used {abilityName} on {target.name}");
+            Debug.Log($"{caster.name} does not have \"{abilityName}\" ability!");
             return;
         }
 
-        Debug.LogError($"{caster.name} does not have \"{abilityName}\" ability!");
+        if (IsAbilityOnCooldown(ability))
+        {
+            Debug.Log($"{abilityName} is on cooldown!");
+            return;
+        }
+
+        if (!IsTargetInRange(caster, target, ability))
+        {
+            Debug.Log($"{target.name} is out of range!");
+            return;
+        }
+
+        ability.Activate(caster, target);
+        StartCooldown(ability);
+
+        Debug.Log($"{caster.name} used {abilityName} on {target.name}");
+    }
+
+    private void AddAbility(Ability ability)
+    {
+        if (!HasAbility(ability.name))
+        {
+            abilityDictionary.Add(ability.name, ability);
+            Debug.Log($"Added {ability.name} to Ability Dictionary");
+        }
+    }
+
+    private bool HasAbility(string abilityName)
+    {
+        return abilityDictionary.ContainsKey(abilityName);
+    }
+
+    private void StartCooldown(Ability ability)
+    {
+        if (cooldownDictionary.TryGetValue(ability, out Coroutine cooldown))
+        {
+            StopCoroutine(cooldown);
+        }
+
+        cooldownDictionary[ability] = StartCoroutine(Cooldown(ability));
+    }
+
+    private void StopCooldown(Ability ability)
+    {
+        if (cooldownDictionary.TryGetValue(ability, out Coroutine cooldown))
+        {
+            StopCoroutine(cooldown);
+        }
+
+        cooldownDictionary.Remove(ability);
+    }
+
+    private bool IsAbilityOnCooldown(Ability ability)
+    {
+        return cooldownDictionary.ContainsKey(ability);
+    }
+
+    private bool IsTargetInRange(Character caster, Character target, Ability ability)
+    {
+        return Vector3.Distance(caster.transform.position, target.transform.position) <= ability.range;
+    }
+
+    private IEnumerator Cooldown(Ability ability)
+    {
+        yield return new WaitForSeconds(ability.cooldown);
+        Debug.Log($"{ability.name} is off cooldown!");
+        StopCooldown(ability);
     }
 }
