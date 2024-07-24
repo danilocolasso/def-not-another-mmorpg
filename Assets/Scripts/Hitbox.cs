0477c895681aaeecacd7df65413ce06965e73821
Danilo <danilo.colasso@gmail.com>
2024-07-24 17:40:30 -0300

    Enhance Hitbox Aweke performance

--- Assets/Scripts/Hitbox.cs
+++ Assets/Scripts/Hitbox.cs
@@ -5,10 +5,15 @@ using UnityEngine.EventSystems;
 [RequireComponent(typeof(CapsuleCollider2D))]
 public class Hitbox : MonoBehaviour
 {
-    public Character Character { get; private set; }
-
+    [SerializeField] private Character character;
+    public Character Character => character;
+    
     private void Awake()
     {
-        Character = GetComponentInParent<Character>();
+        if (character == null)
+        {
+            character = GetComponentInParent<Character>();
+            Debug.LogWarning($"Performance --> {character} Hitbox is null in the Ispector!");
+        }
     }
 }
